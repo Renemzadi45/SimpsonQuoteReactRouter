1855ef83f6c9a1c0b6d4e07c4a2c52c71321181a
Elgur <leroy.maxence@yahoo.com>
2019-10-07 19:30:36 +0200

    QuoteCard.js and .css added and coded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './components/Navbar';
+import QuoteCard from './components/QuoteCard';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <QuoteCard />
     </div>
   );
 }
