d0c263930c6b7ab9d7205341122f45732750fe42
Elgur <leroy.maxence@yahoo.com>
2019-10-07 18:55:17 +0200

    Navbar.css added with purple background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Navbar.css';
+
+const NavBar = () => {
+    return(
+        <nav className="Navbar"><a href="https://en.wikipedia.org/wiki/The_Simpsons" ><img src="https://www.medikamente-per-klick.de/apotheke/wp-content/uploads/2012/08/Fotolia_64348386_XS_copyright-%C2%A9-atoss-Fotolia.com_.jpg" alt='' /></a></nav>
+    );
+}
+
+export default NavBar;
