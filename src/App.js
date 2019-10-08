6bb3111cce32938722872aa7da79498105a25d9b
Elgur <leroy.maxence@yahoo.com>
2019-10-08 14:21:46 +0200

    props added in App.js for quoteCard

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <QuoteCard />
+      <QuoteCard 
+      character="Nelson Muntz"
+      image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+      quote="In theory, Comunisn works! In theory."
+      />
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './QuoteCard.css';
 
-const QuoteCard = () => {
+const QuoteCard = props => {
     return(
         <figure className="QuoteCard">
             <img
-                src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
-                alt="Nelson Muntz" />
+                src={props.image}
+                alt={props.character} />
             <figcaption>
-                <blockquote>
-                Shoplifting is a victimless crime, like punching someone in the dark.
-                </blockquote>
-                <cite>Nelson Muntz</cite>
+                <blockquote>{props.quote}</blockquote>
+                <cite>{props.character}</cite>
             </figcaption>
         </figure>
     );
 }
+
+
 
 export default QuoteCard;
