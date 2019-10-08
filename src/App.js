e8a86bd9c6ed48957f8b5ac929c0b48ccc7176c1
Elgur <leroy.maxence@yahoo.com>
2019-10-08 19:30:11 +0200

    quest react3 done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './components/Navbar';
 import QuoteCard from './components/QuoteCard';
+import QuoteList from './components/QuoteList';
 
 function App() {
   return (
@@ -11,7 +12,15 @@ function App() {
       image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
       quote="In theory, Comunisn works! In theory."
       />
+      <QuoteCard 
+      character="Homer Simpson"
+      image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+      quote="Something that I don't remenber."
+      />
+      <QuoteList />
     </div>
+    
+    
   );
 }
 
