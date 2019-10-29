6b033f5c7b613b4b482f0362adca3e59420604f4
Elgur <leroy.maxence@yahoo.com>
2019-10-29 09:49:08 +0100

    so

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import './QuoteCard.css';
 
-const QuoteCard = props => {
-    return(
-        <figure className="QuoteCard">
-            <img
-                src={props.image}
-                alt={props.character} />
-            <figcaption>
-                <blockquote>{props.quote}</blockquote>
-                <cite>{props.character}</cite>
-            </figcaption>
-        </figure>
-    );
+class QuoteCard extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            favorite: false
+        }
+    }
+    render() {
+        return(
+            <figure className="QuoteCard">
+                <img
+                    src={this.props.image}
+                    alt={this.props.character} />
+                <figcaption>
+                    <blockquote>{this.props.quote}</blockquote>
+                    <p>
+                        <cite>{this.props.character}</cite>
+                        <span
+                        onClick={event => {
+                            // const favUnfav = !this.state.favorite;
+                            this.setState({favorite: !this.state.favorite})
+                            console.log(this.state.favorite);
+                        }}  
+                        className={this.state.favorite ? "is-favorite" : ""}>&#9733;</span>
+                    </p>
+                </figcaption>
+            </figure>
+        );
+    }
 }
 
 
